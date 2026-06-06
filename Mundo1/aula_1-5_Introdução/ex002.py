af9dcfa5d5df5c9ff40e0f31fd7dc022a4e00ffa
Alex R Coelho <alexcoelho.tech@gmail.com>
2026-06-06 16:15:25 -0300

    Correção

--- Mundo1/aula_1-5_Introdução/ex002.py
+++ Mundo1/aula_1-5_Introdução/ex002.py
@@ -1,8 +1,3 @@
 # Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
 nome = input ('Digite seu nome: ')
 print('É um prazer em te conhecer, {}!'.format(nome))
-
-
-# Crie um programa que escreva "Olá, mundo" na tela.
-msg = 'Olá, mundo!'
-print(msg)
